c0929533eb0ea9ad28fb042ba65103e90e429b3d
Cecile Guillot <cecile@guillot-salmacis.com>
2023-06-02 21:36:31 +0200

    Initial commit

---
+++ create_database.sql
@@ -0,0 +1,15 @@
+CREATE DATABASE reddit_posts;
+
+USE reddit_posts;
+
+CREATE TABLE reddit_extraction(
+    id VARCHAR(15),
+    subreddit_name VARCHAR(255),
+    title VARCHAR(255),
+    created_at DATE,
+    nb_comments INT,
+    score INT,
+    text VARCHAR(1000),
+    URL VARCHAR(255),
+    PRIMARY KEY(id)
+);
