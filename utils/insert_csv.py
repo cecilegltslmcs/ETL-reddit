dd853823c41f08708ee212b5fa4e7aa71236fbad
Cecile Guillot <cecile@guillot-salmacis.com>
2023-08-09 22:12:44 +0200

    Update insert_csv

--- utils/insert_csv.py
+++ utils/insert_csv.py
@@ -1,15 +1,48 @@
 import pandas as pd
-import psycopg2
 from sqlalchemy import create_engine
-import authentification_token as auth
+from datetime import datetime
+import os
+from dotenv import load_env
 
-STRING_CONNECTION = auth.string_connection
 
-engine = create_engine(STRING_CONNECTION)
-df = pd.read_csv("data/autism_all.csv")
-df.drop(["created","Unnamed: 0"], axis=1, inplace=True)
-df['id'] = df['id'].values.astype(str)
-df['subreddit_name'] = 'Autism'
-df.drop_duplicates('id', inplace=True)
-df.drop([0, 1], inplace=True)
-df.to_sql("reddit_extraction", engine, if_exists='replace')
+def connect_to_database(string_connection):
+    try:
+        engine = create_engine(string_connection)
+    except:
+        print("Failed to connect to the database! ")
+    return engine
+
+def open_data_csv(path):
+    df = pd.read_csv(path)
+    return df
+
+def transform_data_from_csv(data):
+    data.drop(["created","Unnamed: 0"], axis=1, inplace=True)
+    data['id'] = df['id'].values.astype(str)
+    data['subreddit_name'] = 'Autism'
+    data['date'] = pd.to_datetime(data['date'], unit='s')
+    data.drop_duplicates('id', inplace=True)
+    data.drop([0, 1], inplace=True)
+
+def load_in_database(transformed_data, engine):
+    print("Loading on database...")
+    try:
+        data.to_sql(transformed_data, engine, if_exists='append')
+        return 'Data loaded into database!'
+    except:
+        return 'Error when loading data in database! '
+
+def main(path):
+    try:
+        data = open_data_csv(path)
+        transformed_data = transform_data_from_csv(data)
+        load_in_database(transformed_data, engine)
+        return 'ETL FINISHED!'
+    except:
+        return 'Error during ETL realisation'
+
+if __name__ == '__main__':
+    load_env()
+    STRING_CONNECTION = os.getenv("string_connection")
+    engine = connect_to_database(STRING_CONNECTION)
+    main(path)
